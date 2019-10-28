6df31aff3ef0356bc30f94bf44c0b70eef89fa54
tharindu97 <tharindukavishna@gmail.com>
2019-10-28 09:00:36 +0530

    start_project_step1

---
+++ ecom/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Online Shopping</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="main_wrapper">
+		<div class="header_wrapper">
+			<img id="logo" src="images/profile.png" />
+			<img id="banner" src="images/profile.png"/>
+		</div>
+		
+		<div class= "menubar"> 
+			This is menubar
+		</div>
+
+		<div class = "content_wrapper">
+			<div id ="sidebar"> This is sidebar</div>
+			<div id = "content_area"> This is  content area</div>
+		</div>	
+		
+		<div id = "footer"> This is  the footer</div>
+	</div>
+</body>
+</html>
