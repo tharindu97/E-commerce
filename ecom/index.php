6ddfee8b5b386b7057adf1f2a72d89537f1fc300
tharindu97 <tharindukavishna@gmail.com>
2019-10-29 21:26:26 +0530

    navbar

--- ecom/index.php
+++ ecom/index.php
@@ -5,22 +5,41 @@
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
+<!-- strat main_wrapper -->
 	<div class="main_wrapper">
+
+	<!-- strat header_wrapper -->
 		<div class="header_wrapper">
 			<img id="logo" src="images/profile.png" />
 			<img id="banner" src="images/profile.png"/>
 		</div>
-		
-		<div class= "menubar"> 
-			This is menubar
-		</div>
+	<!-- end header_wrapper -->
 
+	<!-- strat menubar -->	
+		<div class= "menubar"> 
+			<ul id = "menu">
+				<li><a href="#">Home</a></li>
+				<li><a href="#">All Products</a></li>
+				<li><a href="#">My Account</a></li>
+				<li><a href="#">Sign Up</a></li>
+				<li><a href="#">Shopping Cart</a></li>
+				<li><a href="#">Contact</a></li>
+			</ul>
+		</div>
+	<!-- End menubar -->
+
+	<!-- strat content_wrapper -->
 		<div class = "content_wrapper">
 			<div id ="sidebar"> This is sidebar</div>
 			<div id = "content_area"> This is  content area</div>
 		</div>	
-		
+	<!-- strat content_wrapper -->
+
+	<!-- strat footer -->	
 		<div id = "footer"> This is  the footer</div>
+	<!-- strat footer -->
 	</div>
+
+	<!-- End main_wrapper -->
 </body>
 </html>
